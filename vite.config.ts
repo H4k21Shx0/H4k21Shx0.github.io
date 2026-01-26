743a3af04265ea3c3ec518681a5593aff9b00348
Oussada Mohammed Amine <33394604+H4k21Shx0@users.noreply.github.com>
2026-01-27 00:43:21 +0100

    Change base URL in vite.config.ts
    Updated base URL for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      base: '/', // <--- REMPLACEZ par le nom exact de votre projet sur GitHub
+      base: '/H4k21Shx0.github.io', // <--- REMPLACEZ par le nom exact de votre projet sur GitHub
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
